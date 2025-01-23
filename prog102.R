6e5d152cd4ab8f7a26be35d3eef5228cfe571ed1
ciara-gray <ciaragray@ucsb.edu>
2025-01-23 08:36:24 -0800

    upload prog102

--- prog102.R
+++ prog102.R
@@ -1,33 +1,105 @@
 library(marinecs100b)
 
+
+# Writing a utility function ----------------------------------------------
+
+# P1: How did you extract the temperature and exposure from the hottest day?
+# Copy-paste the code here.
+hottest_idx <- which.max(kefj_temperature)
+hottest_time <- kefj_datetime[hottest_idx]
+hottest_site <- kefj_site[hottest_idx]
+hotday_start <- as.POSIXct("2018-07-03 00:00", tz = "Etc/GMT+8")
+hotday_end <- as.POSIXct("2018-07-03 23:59", tz = "Etc/GMT+8")
+hotday_idx <- which(kefj_site == hottest_site &
+                      kefj_datetime >= hotday_start &
+                      kefj_datetime <= hotday_end)
+hotday_datetime <- kefj_datetime[hotday_idx]
+hotday_temperature <- kefj_temperature[hotday_idx]
+hotday_exposure <- kefj_exposure[hotday_idx]
+plot_kefj(hotday_datetime, hotday_temperature, hotday_exposure)
+
+# P2: Fill in the blanks below to write the Alaskan datetime utility function.
+convert_time <- function(date) {
+  result <- as.POSIXct(date, tz = "Etc/GMT+8")
+  return(result)
+}
+
+convert_time("2018-07-03 13:00")
+
+max(1:10)
+my_max <- function(x) {
+  result <- max(x)
+  return(result)
+}
+my_max(1:10)
+
+
 # Extracting data ---------------------------------------------------------
 
-# How did you extract the temperature and exposure from the hottest day?
-# Copy-paste the code here.
+# P3: Make a copy of your code from P1 and edit it to plot the temperature and
+# exposure for "Aialik" on 2012-06-01
+#hottest_idx <- which.max(kefj_temperature)
+#hottest_time <- kefj_datetime[hottest_idx]
+#hottest_site <- kefj_site[hottest_idx]
+day_start <- as.POSIXct("2012-06-01 00:00", tz = "Etc/GMT+8")
+day_end <- as.POSIXct("2012-06-01 23:59", tz = "Etc/GMT+8")
+day_idx <- which(kefj_site == "Aialik" &
+                      kefj_datetime >= day_start &
+                      kefj_datetime <= day_end)
+day_datetime <- kefj_datetime[day_idx]
+day_temperature <- kefj_temperature[day_idx]
+day_exposure <- kefj_exposure[day_idx]
+plot_kefj(day_datetime, day_temperature, day_exposure)
 
-# If you want to change the site and date, what variables would need to change?
+# P4: Make a copy of your code from P3 and edit it to plot the temperature and
+# exposure for "Harris" on 2016-04-05.
+harris_day_start <- as.POSIXct("2016-04-05 00:00", tz = "Etc/GMT+8")
+harris_day_end <- as.POSIXct("2016-04-05 23:59", tz = "Etc/GMT+8")
+harris_day_idx <- which(kefj_site == "Harris" &
+                   kefj_datetime >= harris_day_start &
+                   kefj_datetime <= harris_day_end)
+harris_day_datetime <- kefj_datetime[harris_day_idx]
+harris_day_temperature <- kefj_temperature[harris_day_idx]
+harris_day_exposure <- kefj_exposure[harris_day_idx]
+plot_kefj(harris_day_datetime, harris_day_temperature, harris_day_exposure)
 
-# What would you pick for the temperature extraction function name and parameter
-# names?
+# P5: Compare your solutions for P3 and P4 - what variables changed?
 
+# I changed day_start, day_end, day_idx, day_datetime, day_temperature,
+# day_exposure, also changed "date" and "site" to the requested inputs.
+
+
+# P6: What you would pick for the temperature extraction function and
+# parameters' names?
+
+# extract_temp <- function(???) {
+#   result <- ???()
+#  return(result)
+#}
 
 # Writing extraction functions --------------------------------------------
 
-# Write your temperature extraction function here
+# P7: Fill in the blanks in the code below to write your temperature extraction
+# function.
+extract_temp <- plot_kefj(day_datetime, day_temperature, day_exposure) {
+  ??? <- your_utility_function_name_here(???)
+  ??? <- your_utility_function_name_here(???)
+  ???
+    return(???)
+}
 
-# Write your exposure extraction function here
+# P8: Make a copy of your solution to P7, and edit it to create exposure and
+# datetime extraction functions.
 
-# Write your datetime extraction function here
+# P9: Export your annotated screenshot as a JPEG called "annotated_function.jpg"
+# and add it to your copy of the module repository. (It should be in the same
+# folder as this file.)
 
-# Visualize Nuka Pass temperatures on 2018-07-01
+# P10: Visualize Nuka Pass on July 1, 2018.
 
-# How does the visualization code above compare to what you wrote in PROG101?
+# P11: Save a copy of the Nuka Pass plot as "nuka_pass_2018-07-01.png" in this
+# repo
 
-
-# Functions within functions (optional) -----------------------------------
-
-# How would you parameterize the visualization code into its own function?
-# Specifically, what would you call that function and its parameters?
-
-# Write your wrapper function for plot_kefj(). Call it to compare the
-# visualizations for Nuka Pass on July 1, 2018 versus January 1, 2018.
+# P12: Compare the code you wrote to create the plot in this module to the code
+# you wrote in PROG101. Qualitatively, how do they compare? Which one is easier
+# to read and why?
